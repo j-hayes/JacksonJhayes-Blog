41f2f781438659ce64ac8c17a030606a65b0ab02
jacksonjhayes <jacksonjhayes@gmail.com>
2014-03-28 00:00:21 -0500

    extra file

---
+++ Jackson.Home/Jackson.Home/Controllers/WaiGuoAtHomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Jackson.Home.Models;
+namespace Jackson.Home.Controllers
+{
+    public class WaiGuoAtHomeController : Controller
+    {
+        //
+        // GET: /WaiGuoAtHome/
+
+        public ActionResult Index()
+        {
+            var blogContext = new SiteDataDataContext();
+            List<BlogPost> blogPosts = blogContext.BlogPosts.Take(10).ToList();
+            return View(blogPosts);
+        }
+
+    }
+}
